e65d70b057c79234beeb69e62a27f388a0a57c5b
Jordan <jtaccinelli@gmail.com>
2023-03-21 22:06:29 +1000

    feat: updated to be fixed height

--- apps/frontend/app/components/blocks/ticker/highlight.tsx
+++ apps/frontend/app/components/blocks/ticker/highlight.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function TickerHighlightLayout({ content }: Props) {
   return (
-    <div className="flex gap-2 overflow-hidden bg-accent-600 p-4 text-white">
+    <div className="flex h-16 items-center gap-2 overflow-hidden bg-accent-600 p-4 text-white">
       {Array(2).fill(
         <p className="flex min-w-full flex-shrink-0 animate-scroll justify-around gap-1">
           {Array(10).fill(<p>{content.content}</p>)}
--- apps/frontend/app/components/blocks/ticker/subtle.tsx
+++ apps/frontend/app/components/blocks/ticker/subtle.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function TickerSubtleLayout({ content }: Props) {
   return (
-    <div className="flex gap-2 overflow-hidden bg-gray-900 p-4 text-gray-500">
+    <div className="flex h-16 items-center gap-2 overflow-hidden bg-gray-900 p-4 text-gray-500">
       {Array(2).fill(
         <p className="flex min-w-full flex-shrink-0 animate-scroll justify-around gap-1">
           {Array(10).fill(<p>{content.content}</p>)}
