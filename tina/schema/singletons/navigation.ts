2499977c9490c9a4eab760a0ab766d2835ef6fc9
Jordan <jtaccinelli@gmail.com>
2023-06-23 18:42:01 +1000

    feat: relocated schema to one doc

---
+++ tina/schema/singletons/navigation.ts
@@ -0,0 +1,41 @@
+import type { Collection } from "tinacms";
+
+import { link } from "../objects/link";
+import { defineCustomField } from "../../utils";
+
+export const navigation: Collection = {
+  name: "navigation",
+  label: "Navigation",
+  path: "src/content/navigation",
+  format: "json",
+  ui: {
+    allowedActions: {
+      create: false,
+      delete: false,
+    },
+  },
+  fields: [
+    {
+      type: "string",
+      name: "heading",
+      label: "Heading",
+      required: true,
+    },
+    {
+      type: "string",
+      name: "subheading",
+      label: "Subheading",
+      required: true,
+    },
+    defineCustomField(link, {
+      name: "links",
+      label: "Links",
+      list: true,
+    }),
+    defineCustomField(link, {
+      name: "connect",
+      label: "Connect",
+      list: true,
+    }),
+  ],
+};
